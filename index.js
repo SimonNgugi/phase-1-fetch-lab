f4dbf3b32e8159dabc95e22b2540a886d63bddcc
Simon Ngugi Ngigi <simonngugi88@gmail.com>
2022-08-30 22:33:20 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((resp) => resp.json())
+    .then((json) => {renderBooks(json)});
 }
-
-function renderBooks(books) {
+  function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
